e8bc621b188016778c8d9a42cf030f9fd43e10e4
karvepam <karvendhan_ponnusamy@epam.com>
2026-08-25 16:19:55 +0530

    feat: add employee notification marker helpers

--- employee-leave-management/models/employeeModel.js
+++ employee-leave-management/models/employeeModel.js
@@ -7,6 +7,7 @@ const db = require('../database/db');
 
 /**
  * Find a single employee by their Employee ID.
+ * Includes role and leaveStatusLastSeenAt via SELECT *.
  * @param {string} employeeId
  * @returns {object|undefined}
  */
@@ -24,7 +25,33 @@ function verifyPassword(plainPassword, hashedPassword) {
   return bcrypt.compareSync(plainPassword, hashedPassword);
 }
 
+/**
+ * Update the leave status "last seen" timestamp for an employee.
+ * @param {string} employeeId
+ * @param {string} isoDateTime - ISO 8601 date-time string
+ * @returns {object} - better-sqlite3 run() result
+ */
+function updateLeaveStatusLastSeenAt(employeeId, isoDateTime) {
+  return db
+    .prepare('UPDATE employees SET leaveStatusLastSeenAt = ? WHERE employeeId = ?')
+    .run(isoDateTime, employeeId);
+}
+
+/**
+ * Get the leave status "last seen" timestamp for an employee.
+ * @param {string} employeeId
+ * @returns {string|undefined}
+ */
+function getLeaveStatusLastSeenAt(employeeId) {
+  const row = db
+    .prepare('SELECT leaveStatusLastSeenAt FROM employees WHERE employeeId = ?')
+    .get(employeeId);
+  return row ? row.leaveStatusLastSeenAt : undefined;
+}
+
 module.exports = {
   findByEmployeeId,
   verifyPassword,
+  updateLeaveStatusLastSeenAt,
+  getLeaveStatusLastSeenAt,
 };
